1b7cb717ef26ebc2ef8672323f57ccf454e1fee0
Geet Sethi <geetsethi05@gmail.com>
2024-01-27 12:59:13 +0530

    add about page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,8 +6,18 @@ export default {
       colors: {
         primary: "#4BCAEA",
         background: "#031C40",
-        "background-gradient": "linear-gradient(0deg, #000c09 0%, #031f49 100%)",
-      }
+        "background-gradient":
+          "linear-gradient(0deg, #000c09 0%, #031f49 100%)",
+      },
+      animation: {
+        marquee: "marquee 30s linear infinite reverse",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
     },
   },
   plugins: [],
